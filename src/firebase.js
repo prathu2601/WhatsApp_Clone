9af846f59643d5dbfb6b0e02d2f076c3f138d6e3
prathu2601 <patupmpmdhampu@gmail.com>
2022-02-09 19:18:05 +0530

    whatsapp-clone

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase'
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyCeBTiZo_qApdFEY0dwJFUJq4o6DStjIPA',
-  authDomain: 'whatsapp-clone-af796.firebaseapp.com',
-  projectId: 'whatsapp-clone-af796',
-  storageBucket: 'whatsapp-clone-af796.appspot.com',
-  messagingSenderId: '963526793425',
-  appId: '1:963526793425:web:b5935cdc66d0e1be964df0',
-  measurementId: 'G-XP9WEGMF0G',
+  apiKey: 'AIzaSyCA1K6ngVg2AynVVQk4Ou9Ug2aA05AR8p8',
+  authDomain: 'whatsappclone-a5c27.firebaseapp.com',
+  projectId: 'whatsappclone-a5c27',
+  storageBucket: 'whatsappclone-a5c27.appspot.com',
+  messagingSenderId: '877988151754',
+  appId: '1:877988151754:web:4bc313a9c07d027c0a4f33',
+  measurementId: 'G-80FMR6MTY2',
 }
 
 const firebaseApp = firebase.initializeApp(firebaseConfig)
